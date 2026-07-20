da04f078b9337832f2bea34edd03f64645c3d1ea
mahesh <mahi.240796@gmail.com>
2026-07-20 19:39:03 +0000

    terraform multi region az

--- main.tf
+++ main.tf
@@ -1,7 +1,13 @@
 provider "aws" {
-  region = "us-east-1"
+  region  = "us-east-1"
+  profile = "default"
 }
 
+provider "aws" {
+  region  = "us-east-2"
+  alias   = "ohio"
+  profile = "default"
+}
 # locals {
 #   servers = {
 #     dev  = "t3.small"
@@ -12,15 +18,27 @@ provider "aws" {
 
 
 resource "aws_instance" "webserver1" {
+  provider = aws.ohio
   #   for_each      = local.servers
-  ami = "ami-0476eb1110148a99f"
+  ami = "ami-04ea4e8270c27626c"
   #   instance_type = each.value
   instance_type = "t3.micro"
+  #   region        = "ap-south-1"
   tags = {
     Name = "Webserver1"
     # Name = " Webserver-${each.key}-server"
   }
 }
+
+resource "aws_instance" "webserver2" {
+  ami           = "ami-0476eb1110148a99f"
+  instance_type = "t3.micro"
+  #   region        = "us-east-1"
+  tags = {
+    Name = "Webserver2"
+  }
+}
+
 
 # resource "aws_s3_bucket" "bucket-test" {
 #     bucket = "mahesh-s3bucket-1996test"
