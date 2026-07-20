32181e3c01a569598421542e2fbba591cd13b3ca
mahesh <mahi.240796@gmail.com>
2026-07-20 18:42:53 +0000

    day 2 codes

--- main.tf
+++ main.tf
@@ -2,25 +2,32 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "webserver1" {
-  ami           = "ami-0476eb1110148a99f"
-  instance_type = "t3.small"
+# locals {
+#   servers = {
+#     dev  = "t3.small"
+#     prod = "t3.micro"
+#     uat  = "c7i-flex.large"
+#   }
+# }
 
-tags = {
-    Name = "webserver1"
+
+resource "aws_instance" "webserver1" {
+  #   for_each      = local.servers
+  ami = "ami-0476eb1110148a99f"
+  #   instance_type = each.value
+  instance_type = "t3.micro"
+  tags = {
+    Name = "Webserver1"
+    # Name = " Webserver-${each.key}-server"
   }
 }
 
-resource "aws_s3_bucket" "bucket-test" {
-    bucket = "mahesh-s3bucket-1996test"
-}
+# resource "aws_s3_bucket" "bucket-test" {
+#     bucket = "mahesh-s3bucket-1996test"
+#     depends_on = [aws_instance.webserver1]
+# }
 
-output "instance_name" {
-  value = aws_instance.webserver1.tags["Name"]
-}
-output "instance_type" {
-  value = aws_instance.webserver1.instance_type
-  sensitive = false
-}
-
-
+# output "instance_id" {
+# description = " The id of instance"
+# value = aws_instance.webserver1[*].id
+# }
